e865d612b12f4f4869f866af016db905fe5ce4e0
monika <monikabedn@gmail.com>
2019-12-01 14:59:04 +0100

    Rachunki Klienta

--- src/Bank/core/Client.java
+++ src/Bank/core/Client.java
@@ -39,11 +39,30 @@ public class Client {
         return accountList.add(new Account(accountNumber, accountType));
     }
     public boolean deleteAccount (String accountNumber) {
-        for(Account account : accountList) {
+        for (Account account : accountList) {
             if (account.getAccountNumber().equals(accountNumber)) //equals(accountNumber) zamiast == przy Stringach
                 if (account.getBalance().equals(BigDecimal.ZERO))
                     return accountList.remove(account);
         }
         return false;
     }
+    public boolean deposit(String accountNumber, BigDecimal amount) {
+        for (Account account : accountList) {
+            if (account.getAccountNumber().equals(accountNumber))
+                return account.deposit(amount);
+        }
+                return false;
+        }
+        public boolean withdraw(String accountNumber, BigDecimal amount) {
+            for (Account account : accountList) {
+                if (account.getAccountNumber().equals(accountNumber))
+
+                    return account.withdraw(amount);
+            }
+            return false;
+
+    }
+
+
+
 }
