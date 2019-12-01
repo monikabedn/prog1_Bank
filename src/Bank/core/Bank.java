c6b0c7a612fd015cfd1af23ac91f256d35046c05
monika <monikabedn@gmail.com>
2019-12-01 10:29:55 +0100

    Bank wersja pierwsza

---
+++ src/Bank/core/Bank.java
@@ -0,0 +1,11 @@
+package Bank.core;
+
+public class Bank {
+    private String bankName;
+    public Bank(String bankName){
+        this.bankName =bankName;
+    }
+    public String getBankName() {
+        return bankName;
+    }
+}
