de806f517cdd8bf6f257c0da83e6304647f78a24
monika <monikabedn@gmail.com>
2019-12-01 12:29:35 +0100

    Client Name

--- src/Bank/core/Client.java
+++ src/Bank/core/Client.java
@@ -3,6 +3,25 @@ package Bank.core;
 public class Client {
     private String name, surname;
 
+    private String customerNumber;
+
+    public Client (String name, String surname, String customerNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.customerNumber = customerNumber;
+            }
+
+    public String getName () {
+        return name;
+            }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
     private String pesel;
 
     public String getPesel () {
